d7bf3c7967c1a87e6b269a9b2a20fc248ab7f8f2
VBALAJI2004-pu <vbalajireddy2004@gmail.com>
2025-11-09 17:14:18 +0530

    Replaced vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/PGRKAM_CapstoneProject_CSE_62/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
